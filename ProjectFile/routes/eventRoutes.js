8ce90c519b17b644fa902225845070da032d7f64
votritran <ttvo43@cougarnet.uh.edu>
2025-03-06 16:31:36 -0600

    Event Dynamic page complete

--- ProjectFile/routes/eventRoutes.js
+++ ProjectFile/routes/eventRoutes.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../config/dbconnection');
 router.get('/event', (req, res) => {
-    const events = [
-        {
-            image: '../photo/event/bdaycake.jpg',
-            title: 'Chocolate Cake Making Workshop',
-            date: 'March 12, 2025',
-            time: '2:00 PM - 4:00 PM',
-            description: 'Join us for a hands-on chocolate cake making workshop. Learn tips and tricks from expert bakers!'
-        },
-        {
-            image: '../photo/event/bdaycake.jpg',
-            title: 'Macaron Tasting',
-            date: 'March 20, 2025',
-            time: '1:00 PM - 3:00 PM',
-            description: 'A delightful event to taste a variety of macarons. Come indulge your taste buds!'
-        },
-        // Add more event objects as needed
-    ];
+    
+    connection.query('SELECT * FROM Events', (err, results) => {
+        if (err) {
+            console.error('Error fetching menu items:', err);
+            return res.status(500).send('Error fetching menu items');
+        }
 
-    res.render('event', { events });
+    
+
+    res.render('event', { events: results });
+
+    });
 });
 
 
